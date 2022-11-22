4a3755c86f9be15676c863962e251be8647ff2b9
birddevelper <mostafa_shaeri_tj@yahoo.com>
2022-11-22 13:03:52 -0800

    created

---
+++ productService/src/test/java/mst/example/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mst.example.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
